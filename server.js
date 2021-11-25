48d6b67dcc49defc3a580dddb467a8d69c5a7cde
Misa <91500465+MisaelC02@users.noreply.github.com>
2021-11-25 19:08:17 -0300

    GifdeMai

--- server.js
+++ server.js
@@ -11,32 +11,46 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
-
 let mai = [
-    {Ruta: "/img/mai1.jpg", Titulo: "Sakurajima"},
-    {Ruta: "/img/mai2.jpg", Titulo: "Sakurajima"},
-    {Ruta: "/img/mai4.jpg", Titulo: "Sakurajima"},
-    {Ruta: "/img/mai5.jpg", Titulo: "Sakurajima"},
-    {Ruta: "/img/mai7.jpg", Titulo: "Sakurajima"},
-    {Ruta: "/img/mai8.jpg", Titulo: "Sakurajima"},
-    {Ruta: "/img/mai9.png", Titulo: "Sakurajima"},
-    {Ruta: "/img/mai10.jpg", Titulo: "Sakurajima"},
-    {Ruta: "/img/mai11.jpg", Titulo: "Sakurajima"},
+    { Ruta: "/img/mai1.jpg", Titulo: "Sakurajima" },
+    { Ruta: "/img/mai2.jpg", Titulo: "Sakurajima" },
+    { Ruta: "/img/mai4.jpg", Titulo: "Sakurajima" },
+    { Ruta: "/img/mai5.jpg", Titulo: "Sakurajima" },
+    { Ruta: "/img/mai7.jpg", Titulo: "Sakurajima" },
+    { Ruta: "/img/mai8.jpg", Titulo: "Sakurajima" },
+    { Ruta: "/img/mai9.png", Titulo: "Sakurajima" },
+    { Ruta: "/img/mai10.jpg", Titulo: "Sakurajima" },
+    { Ruta: "/img/mai11.jpg", Titulo: "Sakurajima" },
+    { Ruta: "/img/mai12.jpg", Titulo: "Sakurajima" },
+    { Ruta: "/img/mai13.jpg", Titulo: "Sakurajima" },
+    { Ruta: "/img/mai14.jpg", Titulo: "Sakurajima" },
+    { Ruta: "/img/sakurajima.gif", Titulo: "Sakurajima" },
+    { Ruta: "/img/sakurajima1.gif", Titulo: "Sakurajima" },
+    { Ruta: "/img/sakurajima2.gif", Titulo: "Sakurajima" },
+    { Ruta: "/img/sakurajima3.gif", Titulo: "Sakurajima" },
+    { Ruta: "/img/sakurajima4.gif", Titulo: "Sakurajima" },
+    { Ruta: "/img/sakurajima5.gif", Titulo: "Sakurajima" },
+    { Ruta: "/img/sakurajima6.gif", Titulo: "Sakurajima" },
+    { Ruta: "/img/sakurajima7.gif", Titulo: "Sakurajima" },
+    { Ruta: "/img/sakurajima8.gif", Titulo: "Sakurajima" },
+    { Ruta: "/img/sakurajima9.gif", Titulo: "Sakurajima" },
+    { Ruta: "/img/sakurajima10.gif", Titulo: "Sakurajima" },
+    { Ruta: "/img/sakurajima11.gif", Titulo: "Sakurajima" },
+    { Ruta: "/img/sakurajima12.gif", Titulo: "Sakurajima" },
+    { Ruta: "/img/sakurajima13.gif", Titulo: "Sakurajima" },
 ];
 
 let largo = mai.length;
 
-let amai = [
-    {Ruta: "/img/completamai.jpg"}
-]
+let amai = [{ Ruta: "/img/completamai.jpg" }];
 
 app.get("/hola", (req, res) => {
-    res.render("Index", { mai : mai, largo:largo });
+    res.render("Index", { mai: mai, largo: largo });
     //res.render("pruebas", { p : p});
 });
 
 app.get("/detalles", (req, res) => {
-    res.render("Detalles", {amai : amai} );
+    res.render("Detalles", { amai: amai });
     //res.render("pruebas", { p : p});
 });
 
